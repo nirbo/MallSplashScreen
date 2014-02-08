b2ae38b508f77176d84dfe6b84cd475422dfd724
nirbo <nirbo2@gmail.com>
2014-02-08 20:11:30 +0200

    Added messages rotation AsyncTask and fixed the 5 TextViews layout

--- gen/com/mediatech/mallprojectsplashscreen/R.java
+++ gen/com/mediatech/mallprojectsplashscreen/R.java
@@ -170,26 +170,20 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090013;
-        public static final int informationBarTableRow=0x7f090011;
-        public static final int informationBarTextView=0x7f090012;
-        public static final int loadingCircleProgressBar=0x7f090010;
-        public static final int loadingCircleTableRow=0x7f09000f;
+        public static final int action_settings=0x7f09000d;
+        public static final int informationBarTableRow=0x7f09000b;
+        public static final int informationBarTextView=0x7f09000c;
+        public static final int loadingCircleProgressBar=0x7f09000a;
+        public static final int loadingCircleTableRow=0x7f090009;
         public static final int logoImageView=0x7f090002;
         public static final int logoTableRow=0x7f090001;
         public static final int mainTableLayout=0x7f090000;
-        public static final int messageInnerTableRow0=0x7f090005;
-        public static final int messageInnerTableRow1=0x7f090007;
-        public static final int messageInnerTableRow2=0x7f090009;
-        public static final int messageInnerTableRow3=0x7f09000b;
-        public static final int messageInnerTableRow4=0x7f09000d;
-        public static final int messageTextView0=0x7f090006;
-        public static final int messageTextView1=0x7f090008;
-        public static final int messageTextView2=0x7f09000a;
-        public static final int messageTextView3=0x7f09000c;
-        public static final int messageTextView4=0x7f09000e;
-        public static final int messagesInnerTableLayout=0x7f090004;
-        public static final int messagesTableRow=0x7f090003;
+        public static final int messageTextView0=0x7f090004;
+        public static final int messageTextView1=0x7f090005;
+        public static final int messageTextView2=0x7f090006;
+        public static final int messageTextView3=0x7f090007;
+        public static final int messageTextView4=0x7f090008;
+        public static final int messagesInnerLinearLayout=0x7f090003;
     }
     public static final class layout {
         public static final int splash_screen_main=0x7f030000;
--- src/com/mediatech/mallprojectsplashscreen/MainActivity.java
+++ src/com/mediatech/mallprojectsplashscreen/MainActivity.java
@@ -1,11 +1,19 @@
 package com.mediatech.mallprojectsplashscreen;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -23,12 +31,23 @@ public class MainActivity extends Activity {
 	private CountDownTimer mCountDownTimer;
 	private Handler mCountDownHandler;
 	private Thread mCountDownThread;
+	private Boolean mCountDownThreadFinished = false;
 	
 	private TextView mInfoTextView;
 	private ImageView mLogoImageView;
 	private Drawable mLogoImage;
 	private Bitmap mLogoBitmap;
 	private Bitmap mResizedLogoBitmap;
+	
+	private TextView messageTextView0;
+	private TextView messageTextView1;
+	private TextView messageTextView2;
+	private TextView messageTextView3;
+	private TextView messageTextView4;
+	
+	private GetMessagesAsyncTask mGetMessagesAsyncTask = new GetMessagesAsyncTask();
+	private List<String> mInfoTextsArrayList = new ArrayList<String>();
+	private List<TextView> mTextViewArrayList = new ArrayList<TextView>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,19 +58,44 @@ public class MainActivity extends Activity {
 		mLogoBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cinemall_logo);
 		mLoadingCircle  = (ProgressBar) findViewById(R.id.loadingCircleProgressBar);
 		mInfoTextView = (TextView) findViewById(R.id.informationBarTextView);
+		
+		messageTextView0 = (TextView) findViewById(R.id.messageTextView0);
+		mTextViewArrayList.add(messageTextView0);
+		messageTextView1 = (TextView) findViewById(R.id.messageTextView1);
+		mTextViewArrayList.add(messageTextView1);
+		messageTextView2 = (TextView) findViewById(R.id.messageTextView2);
+		mTextViewArrayList.add(messageTextView2);
+		messageTextView3 = (TextView) findViewById(R.id.messageTextView3);
+		mTextViewArrayList.add(messageTextView3);
+		messageTextView4 = (TextView) findViewById(R.id.messageTextView4);
+		mTextViewArrayList.add(messageTextView4);
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		
-		getActionBar().hide();		
+		// --- Begin execution ---
+		getActionBar().hide();
 		prepareSplashLogoImage();
+		mInfoTextsArrayList = populateInfoTextArrayList(100);
 		
 		// Instantiate the Handler and the Thread then execute the thread.
 		mCountDownHandler = new Handler();
 		mCountDownThread = new Thread(new CountDownThread());
 		mCountDownHandler.post(mCountDownThread);
+		// ---------------------------------------------------------------
+		
+		// Execute the GetMessages AsyncTask
+//		try {
+//			mGetMessagesAsyncTask.execute();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			Log.e(LOGTAG, "ERROR: Could not retrieve splash-screen messages from the server; Splash messages will NOT be displayed!");
+//		}
+		// ---------------------------------------------------------------
+		
+		scheduleGetMessagesAsyncTask();
 	}
 	
 	@Override
@@ -59,14 +103,16 @@ public class MainActivity extends Activity {
 		super.onPause();
 		
 		mCountDownTimer.cancel();
+		mCountDownThreadFinished = true;
 	}
 	
 	@Override
 	public void onBackPressed() {
 		super.onBackPressed();
 		
-		// Stop the CountDownTimer running in CountDownThread and finish this activity.
+		// Stop the CountDownTimer, GetMessages AsyncTask, and finish this activity.
 		mCountDownTimer.cancel();
+		mCountDownThreadFinished = true;
 		this.finish();
 	}
 	
@@ -88,12 +134,98 @@ public class MainActivity extends Activity {
 		}
 	}
 	
+	// Get the splash messages from the server and display them in a rotation.
+	private void getSplashMessages() {
+				
+		String mNextMessageToDisplay = null;
+		TextView mNextTextViewToUse = null;
+			
+		int mMessagesCounter = 0;
+		int mTextViewCounter = 0;
+		int mMessagesArrayListSize = mInfoTextsArrayList.size();
+		int mTextViewArrayListSize = mTextViewArrayList.size();
+				
+		// Verify the messages ArrayList has been successfully populated and execute message-rotation logic.
+		mMessagesArrayListSize = mInfoTextsArrayList.size();
+		mTextViewArrayListSize = mTextViewArrayList.size();
+		
+		while (mTextViewCounter < 5) {
+			mMessagesCounter = mTextViewCounter;
+			
+			// Get new messages to display if all previously pulled messages have been displayed and the ArrayList is empty now.
+			if (mInfoTextsArrayList.isEmpty()) {
+				populateInfoTextArrayList(100);
+			}
+			
+			// Get next TextView to use and message contents and display them in the UI
+			mNextMessageToDisplay = mInfoTextsArrayList.get(mMessagesCounter);
+			mNextTextViewToUse = mTextViewArrayList.get(mTextViewCounter);
+			mNextTextViewToUse.setText(mNextMessageToDisplay);
+			
+			mInfoTextsArrayList.remove(mMessagesCounter);
+			mTextViewCounter++;
+			
+			// Debugging
+			Log.i(LOGTAG, "Next Message: " + mMessagesCounter +  " " + mInfoTextsArrayList.get(mMessagesCounter));
+		}
+	}
+	
+	// Random String Generator --- temporary for simulating the MessageTextView contents //
+	private static final String RANDOM_STRING_CHARACTERS ="0123456789qwertyuiopasdfghjklzxcvbnm ";
+		
+	private String getRandomString(final int sizeOfRandomString) {
+		final Random mRandomGenerator = new Random();
+		final StringBuilder mRandomStringBuilder = new StringBuilder();
+		for (int i = 0; i < sizeOfRandomString; i++) {
+			mRandomStringBuilder.append(RANDOM_STRING_CHARACTERS.charAt(mRandomGenerator.nextInt(RANDOM_STRING_CHARACTERS.length())));
+		}
+
+		return mRandomStringBuilder.toString();
+	}
+			
+	public ArrayList<String> populateInfoTextArrayList(int amountOfEntries) {
+		ArrayList<String> mTempArrayList = new ArrayList<String>();
+		String mTempString;
+		
+		while (mTempArrayList.size() < amountOfEntries) {
+			mTempString = getRandomString(125);
+			mTempArrayList.add(mTempString);
+		}
+		
+		return mTempArrayList;
+	}
+	
+	public void scheduleGetMessagesAsyncTask() {
+		final Handler mScheduleAsyncTaskHandler = new Handler();
+		Timer mTimer = new Timer();
+		final long REPEAT_INTERVAL = 7000;
+		TimerTask executeAsyncTask = new TimerTask() {
+
+			@Override
+			public void run() {
+				mScheduleAsyncTaskHandler.post(new Runnable() {
+					
+					@Override
+					public void run() {
+						mGetMessagesAsyncTask = new GetMessagesAsyncTask();
+						if (mCountDownThreadFinished) {
+							mGetMessagesAsyncTask.cancel(true);
+						}
+						mGetMessagesAsyncTask.execute();
+					}
+				});
+			}
+		};
+		mTimer.schedule(executeAsyncTask, 0, REPEAT_INTERVAL);
+	}
 	
 	
 	
-	 //	  ----------------------------------------	 \\
-	///// ----- CountDown Thread Inner Class ----- \\\\\
-   //	  ----------------------------------------	   \\
+/*  
+    ------------------------------------------------	 
+	----- CountDown Thread Inner Class -----
+	------------------------------------------------
+*/
 	private class CountDownThread implements Runnable {
 
 		int mCountDownLength = 30000;
@@ -130,6 +262,7 @@ public class MainActivity extends Activity {
 					// Hide the "Loading" circle and print "Done!" upon completion
 					mLoadingCircle.setVisibility(View.INVISIBLE);
 					mInfoTextView.setText(R.string.loading_complete);
+					mCountDownThreadFinished = true;
 				}
 			}.
 			start();
@@ -138,15 +271,37 @@ public class MainActivity extends Activity {
 	}	
 	
 	
+/*  
+    ------------------------------------------------	 
+	----- Splash Message AsyncTask Inner Class -----
+	------------------------------------------------
+		
+	This class fetches the messages to display in the Splash Screen from a server and presents them
+	In a rotation on screen in the relevant TextViews.
+*/
+
+	private class GetMessagesAsyncTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			
+			getSplashMessages();
+		}
+	}
+
+
+	
+	
+	
+	
 }
-
-
-
-
-
-
-
-
 
 
 
